f1c84ebc1dab1aaf59a148a0c47c5930493df16f
Dharmendra Patel <dharmendrapatel1911996@gmail.com>
2025-05-18 22:03:23 +0530

    Update settings.py

--- oneclickelectronics/settings.py
+++ oneclickelectronics/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"  # below this add the following line
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 EFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -138,8 +139,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'oneclickelectronicsecomm@gmail.com'
 EMAIL_HOST_PASSWORD = 'Dhamo@20196'
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
